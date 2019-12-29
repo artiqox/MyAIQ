4cc408c6379d392ad53cbd1128e6ee4435152bae
znafca <36641122+znafca@users.noreply.github.com>
2019-12-29 11:27:15 +0100

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -6,15 +6,17 @@ Copyright (c) 2019 - present AppSeed.us
 
 from flask_wtf          import FlaskForm
 from flask_wtf.file     import FileField, FileRequired
-from wtforms            import StringField, TextAreaField, SubmitField, PasswordField
+from wtforms            import StringField, TextAreaField, SubmitField, PasswordField, RadioField
 from wtforms.validators import InputRequired, Email, DataRequired
 
 class LoginForm(FlaskForm):
+	type        = RadioField   (u'Login with:', choices=[('TG','telegram'),('TW','twitter')], default="TG", validators=[DataRequired()])
 	username    = StringField  (u'Username'        , validators=[DataRequired()])
 	password    = PasswordField(u'Password'        , validators=[DataRequired()])
 
 class RegisterForm(FlaskForm):
+	type        = RadioField   ('Register with:', choices=[('TG','telegram'),('TW','twitter')], default="TG", validators=[DataRequired()])
 	name        = StringField  (u'Name'      )
 	username    = StringField  (u'Username'  , validators=[DataRequired()])
 	password    = PasswordField(u'Password'  , validators=[DataRequired()])
-	email       = StringField  (u'Email'     , validators=[DataRequired(), Email()])
+	#email       = StringField  (u'Email'     , validators=[DataRequired(), Email()])
