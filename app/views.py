7881367455802503a5851c86926669937cd6090b
znafca <36641122+znafca@users.noreply.github.com>
2019-12-29 11:22:41 +0100

    Update views.py

--- app/views.py
+++ app/views.py
@@ -5,7 +5,10 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 # Python modules
-import os, logging 
+import os, logging, requests, json
+import string
+from random import *
+from datetime import datetime
 
 # Flask modules
 from flask               import render_template, request, url_for, redirect, send_from_directory
@@ -14,8 +17,55 @@ from werkzeug.exceptions import HTTPException, NotFound, abort
 
 # App modules
 from app        import app, lm, db, bc
-from app.models import User
+from app.models import User, Verification
 from app.forms  import LoginForm, RegisterForm
+
+aie_node_api = 'http://localhost:6870/nxt'
+aiq_asset_id = '5384720030523531536'
+genesis_block_ts = 1568469599
+
+def get_aiq_balance(aie_account):
+    aiereponse = requests.post(aie_node_api+'?requestType=getAccountAssets', data={'account':aie_account, 'asset':aiq_asset_id})
+    json_aieresponse = aiereponse.json()
+    try:
+        balance = float(json_aieresponse["quantityQNT"])/100
+    except Exception as e:
+        balance = 0.0
+    return balance
+
+def get_aie_balance(aie_account):
+    aiereponse = requests.post(aie_node_api+'?requestType=getBalance', data={'account':aie_account})
+    json_aieresponse = aiereponse.json()
+    try:
+        balance = float(json_aieresponse["balanceNQT"])/100000000
+    except Exception as e:
+        balance = 0.0
+    return balance
+
+def get_account_transactions(aie_account):
+    transactions = {}
+    aiereponse = requests.post(aie_node_api+'?requestType=getBlockchainTransactions', data={'account':aie_account, 'lastIndex':10})
+    json_aieresponse = aiereponse.json()
+    try:
+        transactions_raw = json_aieresponse["transactions"]
+    except Exception as e:
+        transactions_raw = 0
+    
+    if transactions_raw != 0:
+        for transaction in transactions_raw:
+            transactions.setdefault(datetime.utcfromtimestamp(genesis_block_ts+int(transaction["timestamp"])).strftime('%Y-%m-%d %H:%M:%S'),{})['sender'] = transaction["senderRS"]
+            transactions.setdefault(datetime.utcfromtimestamp(genesis_block_ts+int(transaction["timestamp"])).strftime('%Y-%m-%d %H:%M:%S'),{})['recipient'] = transaction["recipientRS"]
+            transactions.setdefault(datetime.utcfromtimestamp(genesis_block_ts+int(transaction["timestamp"])).strftime('%Y-%m-%d %H:%M:%S'),{})['fee'] = str(float(transaction["feeNQT"])/100000000)
+            if transaction["type"] == 0 and transaction["subtype"] == 0:
+                transactions.setdefault(datetime.utcfromtimestamp(genesis_block_ts+int(transaction["timestamp"])).strftime('%Y-%m-%d %H:%M:%S'),{})['amount'] = str(float(transaction["amountNQT"])/100000000)+" AIE"
+            elif transaction["type"] == 2 and transaction["subtype"] == 1:
+                if transaction["attachment"]["asset"] == aiq_asset_id:
+                    transactions.setdefault(datetime.utcfromtimestamp(genesis_block_ts+int(transaction["timestamp"])).strftime('%Y-%m-%d %H:%M:%S'),{})['amount'] = str(float(transaction["attachment"]["quantityQNT"])/100)+" AIQ"
+                else:
+                    transactions.setdefault(datetime.utcfromtimestamp(genesis_block_ts+int(transaction["timestamp"])).strftime('%Y-%m-%d %H:%M:%S'),{})['amount'] = "NA"
+            else:
+                transactions.setdefault(datetime.utcfromtimestamp(genesis_block_ts+int(transaction["timestamp"])).strftime('%Y-%m-%d %H:%M:%S'),{})['amount'] = "NA"
+    return transactions
 
 # provide login manager with load_user callback
 @lm.user_loader
@@ -47,26 +97,38 @@ def register():
         # assign form data to variables
         username = request.form.get('username', '', type=str)
         password = request.form.get('password', '', type=str) 
-        email    = request.form.get('email'   , '', type=str) 
+        type    = request.form.get('type'   , '', type=str) 
+        myaiq_username = type+"-"+username
+        if type == "TG":
+            allchar = string.ascii_letters + string.digits
+            user_tweet = "".join(choice(allchar) for x in range(randint(5, 5)))
+            confirm_my_stuff = "/verifyme " + user_tweet
+            verification = Verification(username=myaiq_username, confirm_my_stuff=user_tweet)
+            verification.set_password(password)
+            db.session.add(verification)
+            db.session.commit()
+            msg = 'Please use telegram, talk to @MyAiqBot and tell him "' + confirm_my_stuff + '"'
+            #flash(totweet)
+            #return redirect(url_for('login'))
 
-        # filter User out of database through username
-        user = User.query.filter_by(user=username).first()
-
-        # filter User out of database through username
-        user_by_email = User.query.filter_by(email=email).first()
-
-        if user or user_by_email:
-            msg = 'Error: User exists!'
-        
-        else:         
-
-            pw_hash = password #bc.generate_password_hash(password)
-
-            user = User(username, email, pw_hash)
-
-            user.save()
-
-            msg = 'User created, please <a href="' + url_for('login') + '">login</a>'     
+#        # filter User out of database through username
+#        user = User.query.filter_by(user=username).first()
+#
+#        # filter User out of database through username
+#        user_by_email = User.query.filter_by(email=email).first()
+#
+#        if user or user_by_email:
+#            msg = 'Error: User exists!'
+#        
+#        else:         
+#
+#            pw_hash = password #bc.generate_password_hash(password)
+#
+#            user = User(username, email, pw_hash)
+#
+#            user.save()
+#
+#            msg = 'User created, please <a href="' + url_for('login') + '">login</a>'     
 
     else:
         msg = 'Input error'     
@@ -89,14 +151,16 @@ def login():
         # assign form data to variables
         username = request.form.get('username', '', type=str)
         password = request.form.get('password', '', type=str) 
+        type    = request.form.get('type'   , '', type=str) 
+        myaiq_username = type+"-"+username
 
         # filter User out of database through username
-        user = User.query.filter_by(user=username).first()
+        user = User.query.filter_by(username=myaiq_username).first()
 
         if user:
             
             #if bc.check_password_hash(user.password, password):
-            if user.password == password:
+            if user.check_password(password):
                 login_user(user)
                 return redirect(url_for('index'))
             else:
@@ -122,9 +186,13 @@ def index(path):
         # Temporary solution to solve the dependencies
         if path.endswith(('.png', '.svg' '.ttf', '.xml', '.ico', '.woff', '.woff2')):
             return send_from_directory(os.path.join(app.root_path, 'static'), path)    
+        aie_account = current_user.aie_account
+        aiq_balance=get_aiq_balance(aie_account)
+        aie_balance=get_aie_balance(aie_account)
+        transactions=get_account_transactions(aie_account)
 
         # try to match the pages defined in -> pages/<input file>
-        return render_template( 'pages/'+path )
+        return render_template( 'pages/'+path, aiq_balance=aiq_balance, aie_balance=aie_balance, transactions=transactions )
 
     except:
         
